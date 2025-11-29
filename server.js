21299276ce5914259a504e47ad8aeb7d8593c793
LunarSamurai <josephcraig757@gmail.com>
2025-11-29 04:33:47 -0500

    changes update

--- server.js
+++ server.js
@@ -1,111 +1,164 @@
-const dgram = require('dgram');
-const http = require('http');
+var _type = async_load[? "type"];
+var _id = async_load[? "id"];
 
-const PORT = process.env.PORT || 3000;
-const UDP_PORT = 5000;
+if (_id != global.ws_socket) exit;
 
-// === HTTP SERVER (for Fly.io health checks) ===
-const httpServer = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Game Matchmaker Server Running\n');
-});
-
-httpServer.listen(PORT, () => {
-    console.log(`HTTP health check server running on port ${PORT}`);
-});
-
-// === UDP MATCHMAKING SERVER ===
-const udpServer = dgram.createSocket('udp4');
-
-const lobbies = new Map();
-
-udpServer.on('message', (msg, rinfo) => {
-    try {
-        const data = JSON.parse(msg.toString());
-        const clientAddr = `${rinfo.address}:${rinfo.port}`;
-        
-        console.log(`From ${clientAddr}:`, data);
-        
-        switch (data.type) {
-            case 'create':
-                const code = Math.random().toString(36).substring(2, 8).toUpperCase();
-                
-                lobbies.set(code, {
-                    host: { ip: rinfo.address, port: rinfo.port, name: data.name },
-                    guest: null,
-                    created: Date.now()
-                });
-                
-                const response = JSON.stringify({ type: 'created', code: code });
-                udpServer.send(response, rinfo.port, rinfo.address);
-                
-                console.log(`Lobby created: ${code}`);
-                break;
-                
-            case 'join':
-                const lobby = lobbies.get(data.code);
-                
-                if (!lobby) {
-                    const err = JSON.stringify({ type: 'error', msg: 'Lobby not found' });
-                    udpServer.send(err, rinfo.port, rinfo.address);
-                    return;
-                }
-                
-                if (lobby.guest) {
-                    const err = JSON.stringify({ type: 'error', msg: 'Lobby full' });
-                    udpServer.send(err, rinfo.port, rinfo.address);
-                    return;
-                }
-                
-                lobby.guest = { ip: rinfo.address, port: rinfo.port, name: data.name };
-                
-                // Tell guest about host
-                const toGuest = JSON.stringify({
-                    type: 'peer',
-                    ip: lobby.host.ip,
-                    port: lobby.host.port,
-                    name: lobby.host.name
-                });
-                udpServer.send(toGuest, rinfo.port, rinfo.address);
-                
-                // Tell host about guest
-                const toHost = JSON.stringify({
-                    type: 'peer',
-                    ip: lobby.guest.ip,
-                    port: lobby.guest.port,
-                    name: lobby.guest.name
-                });
-                udpServer.send(toHost, lobby.host.port, lobby.host.ip);
-                
-                console.log(`Peer exchange for lobby: ${data.code}`);
-                
-                // Clean up lobby
-                lobbies.delete(data.code);
-                break;
-                
-            case 'ping':
-                const pong = JSON.stringify({ type: 'pong' });
-                udpServer.send(pong, rinfo.port, rinfo.address);
-                break;
+switch (_type) {
+    case network_type_non_blocking_connect:
+        if (async_load[? "succeeded"]) {
+            global.ws_connected = true;
+            show_debug_message("Connected to relay server!");
+            
+            with (obj_character_menu) {
+                mp_status_message = "Connected to server";
+            }
+            
+            if (global.ws_pending_action == "create") {
+                ws_create_lobby();
+            } else if (global.ws_pending_action == "join") {
+                ws_join_lobby(global.ws_pending_code);
+            }
+            global.ws_pending_action = "";
+            global.ws_pending_code = "";
+        } else {
+            show_debug_message("Failed to connect to relay server");
+            with (obj_character_menu) {
+                mp_state = "idle";
+                mp_status_message = "Failed to connect!";
+            }
         }
-    } catch (e) {
-        console.error('Error:', e);
-    }
-});
-
-udpServer.on('listening', () => {
-    console.log(`UDP Matchmaking server running on port ${UDP_PORT}`);
-});
-
-// Clean up old lobbies every minute
-setInterval(() => {
-    const now = Date.now();
-    for (const [code, lobby] of lobbies) {
-        if (now - lobby.created > 300000) { // 5 minutes
-            lobbies.delete(code);
-            console.log(`Cleaned up old lobby: ${code}`);
+        break;
+        
+    case network_type_data:
+        var _buffer = async_load[? "buffer"];
+        buffer_seek(_buffer, buffer_seek_start, 0);
+        var _json_str = buffer_read(_buffer, buffer_text);
+        
+        show_debug_message("Received: " + _json_str);
+        
+        try {
+            var _msg = json_parse(_json_str);
+            
+            switch (_msg.type) {
+                case "lobby_created":
+                    global.ws_lobby_code = _msg.code;
+                    global.ws_is_host = true;
+                    
+                    with (obj_character_menu) {
+                        mp_state = "hosting";
+                        mp_status_message = "Lobby created!";
+                        lobby_code = _msg.code;
+                    }
+                    show_debug_message("Lobby created: " + _msg.code);
+                    break;
+                    
+                case "join_success":
+                    global.ws_is_connected = true;
+                    global.ws_is_host = false;
+                    
+                    if (instance_exists(obj_hero)) {
+                        global.ws_other_player = instance_create_layer(obj_hero.x + 50, obj_hero.y, "Instances", obj_player_other);
+                        global.ws_other_player.player_name = _msg.hostName;
+                    }
+                    
+                    with (obj_character_menu) {
+                        mp_state = "connected";
+                        mp_status_message = "Connected!";
+                        connected_player_name = _msg.hostName;
+                    }
+                    show_debug_message("Joined lobby!");
+                    break;
+                    
+                case "join_failed":
+                    with (obj_character_menu) {
+                        mp_state = "idle";
+                        mp_status_message = "Failed: " + _msg.reason;
+                    }
+                    show_debug_message("Join failed: " + _msg.reason);
+                    break;
+                    
+                case "player_joined":
+                    global.ws_is_connected = true;
+                    
+                    if (instance_exists(obj_hero)) {
+                        global.ws_other_player = instance_create_layer(obj_hero.x + 50, obj_hero.y, "Instances", obj_player_other);
+                        global.ws_other_player.player_name = _msg.playerName;
+                    }
+                    
+                    with (obj_character_menu) {
+                        mp_state = "connected";
+                        mp_status_message = "Player joined!";
+                        connected_player_name = _msg.playerName;
+                    }
+                    show_debug_message("Player joined: " + _msg.playerName);
+                    break;
+                    
+                case "player_left":
+                    global.ws_is_connected = false;
+                    
+                    if (instance_exists(global.ws_other_player)) {
+                        instance_destroy(global.ws_other_player);
+                        global.ws_other_player = noone;
+                    }
+                    
+                    with (obj_character_menu) {
+                        if (global.ws_is_host) {
+                            mp_state = "hosting";
+                            mp_status_message = "Player left";
+                        } else {
+                            mp_state = "idle";
+                            mp_status_message = "Host left";
+                            lobby_code = "";
+                        }
+                        connected_player_name = "";
+                    }
+                    show_debug_message("Player left");
+                    break;
+                    
+                case "game_data":
+                    var _data = _msg.data;
+                    
+                    if (_data.t == "pos") {
+                        if (instance_exists(global.ws_other_player)) {
+                            global.ws_other_player.target_x = _data.x;
+                            global.ws_other_player.target_y = _data.y;
+                            global.ws_other_player.aim_angle = _data.a;
+                            global.ws_other_player.sprite_index = _data.s;
+                            global.ws_other_player.image_xscale = _data.xs;
+                        }
+                    }
+                    else if (_data.t == "bullet") {
+                        var _bullet = instance_create_layer(_data.x, _data.y, "Instances", obj_hero_bullet);
+                        _bullet.direction = _data.dir;
+                        _bullet.speed = 28;
+                        _bullet.image_angle = _data.dir;
+                        _bullet.image_xscale = 0.375;
+                        _bullet.image_yscale = 0.375;
+                        _bullet.from_network = true;
+                    }
+                    break;
+            }
+        } catch (e) {
+            show_debug_message("Parse error: " + string(e));
         }
-    }
-}, 60000);
-
-udpServer.bind(UDP_PORT);
+        break;
+        
+    case network_type_disconnect:
+        global.ws_connected = false;
+        global.ws_is_connected = false;
+        global.ws_socket = -1;
+        
+        if (instance_exists(global.ws_other_player)) {
+            instance_destroy(global.ws_other_player);
+            global.ws_other_player = noone;
+        }
+        
+        with (obj_character_menu) {
+            mp_state = "idle";
+            mp_status_message = "Disconnected";
+            connected_player_name = "";
+            lobby_code = "";
+        }
+        show_debug_message("Disconnected from server");
+        break;
+}
